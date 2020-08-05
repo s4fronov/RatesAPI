614965f3cff1225217e8bd576ba45ade7da11567
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-08-05 12:43:44 +0300

    Edit Convert DateTime To string

--- Rates.API/Rates.API/Program.cs
+++ Rates.API/Rates.API/Program.cs
@@ -23,34 +23,25 @@ namespace Rates.API
 
         public static void Main(string[] args)
         {
-            Rates currencies = new Rates();                    
+            Rates currencies = new Rates();
             string Filepath = @"D:\currency.txt";
             var startTimeSpan = TimeSpan.Zero;
             var periodTimeSpan = TimeSpan.FromMinutes(60);
 
             var timer = new Timer((e) =>
             {
-            string currencyRates = RatesRequest.GetRates();
-            string[] data = Regex.Split(currencyRates, @"\D+");
-            currencies.time = new DateTime(1970, 1, 1).AddSeconds(Convert.ToInt32(data[1]));
-            var time = Convert.ToString(currencies.time);
-            currencies.USD = Convert.ToDouble(data[5] + "," + data[6]);
-            currencies.RUB = Convert.ToDouble(data[7] + "," + data[8]);
-            currencies.JPY = Convert.ToDouble(data[9] + "," + data[10]);
-            string currenciesData = "DateOfChanges: "+ time + "; USD = " +(data[5] + "," + data[6]) + "; RUB = " + (data[7] + "," + data[8]) + "; JPY = " + (data[9] + "," + data[10]);
-            var jsonContent = new StringContent(JsonConvert.SerializeObject(currencies), Encoding.UTF8, "application/json");
-            File.AppendAllText(Filepath, currenciesData +"\n");
-                
-
+                string currencyRates = RatesRequest.GetRates();
+                string[] data = Regex.Split(currencyRates, @"\D+");
+                currencies.Time = new DateTime(1970, 1, 1).AddSeconds(Convert.ToInt32(data[1]));
+                var time = currencies.Time.ToString("dd.MM.yyyy HH:mm:ss");
+                currencies.USD = Convert.ToDouble(data[5] + "," + data[6]);
+                currencies.RUB = Convert.ToDouble(data[7] + "," + data[8]);
+                currencies.JPY = Convert.ToDouble(data[9] + "," + data[10]);
+                string currenciesData = "DateOfChanges: " + time + "; USD = " + (data[5] + "," + data[6]) + "; RUB = " + (data[7] + "," + data[8]) + "; JPY = " + (data[9] + "," + data[10]);
+                var jsonContent = new StringContent(JsonConvert.SerializeObject(currencies), Encoding.UTF8, "application/json");
+                File.AppendAllText(Filepath, currenciesData + "\n");
             }, null, startTimeSpan, periodTimeSpan);
-            
-
             Console.ReadKey();
         }
-
-       
-
-
-
     }
 }
