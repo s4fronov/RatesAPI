7c704a67228e31750aa96f7db2efef3a75730eb2
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-08-10 09:20:36 +0300

    try to do rabbitmq

--- Rates.API/Rates.API/Program.cs
+++ Rates.API/Rates.API/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
+using System.Text;
 using System.Threading;
+using MassTransit;
 using Newtonsoft.Json;
 
 namespace Rates.API
@@ -9,8 +11,40 @@ namespace Rates.API
     {
         //public static object JsonConvert { get; private set; }
 
-        public static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string host = "host";
+            string vhost = "vhost";
+            string username = "username";
+            string password = "password";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                IRabbitMqHost rmqHost = cfg.Host(host, vhost, h =>
+                {
+                    h.Username(username);
+                    h.Password(password);
+                });
+            });
+
+            bus.Start();
+
+            Console.WriteLine("Нажмите Enter, чтобы опубликовать событийное сообщение.");
+            Console.ReadKey();
+
+            await bus.Publish<EventMessage>(new { Text = "Событийное сообщение: приходит ко всем потребителям." });
+
+            Console.WriteLine("Нажмите Enter, чтобы опубликовать соревновательное сообщение.");
+            Console.ReadKey();
+
+            await bus.Publish<CompetingConsumersMessage>(new { Text = "Соревновательное сообщение: принимается только одним из подписчиков очереди." });
+
+            Console.ReadKey();
+
+            bus.Stop();
             ExchangeRateModel currencies = new ExchangeRateModel();
             string Filepath = @"D:\currency.txt";
             var startTimeSpan = TimeSpan.Zero;
